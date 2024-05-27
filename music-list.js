b72ce4f6b31894ceeeee62cbcd8e9bca92b9de51
RaoSaheb7777 <160231718+RaoSaheb7777@users.noreply.github.com>
2024-05-27 21:14:09 +0530

    Add files via upload
    MUSIC PLAYER using HTML, CSS and JavaScript

---
+++ music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Soul-Mate",
+    artist: "Badsha x Arjit Singh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Yimmy Yimmy",
+    artist: "Shreya Ghosal x Tayc",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Born to Shine",
+    artist: "Diljit Dosanjh",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Gulabi Sadi Ani Lali",
+    artist: "Sanju Rathod",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Kate Nahi Katte X saath",
+    artist: "Various Artists",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Tere Liye X Eternity",
+    artist: "Atif Aslam",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
